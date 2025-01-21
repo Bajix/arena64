1d0e593553e185e67014ea87576dc4e6a005755c
Thomas Sieverding <Thomas@bajix.com>
2025-01-21 12:26:34 -0600

    Update benchmarks

--- bench/src/lib.rs
+++ bench/src/lib.rs
@@ -1,55 +1,24 @@
-use std::{
-    mem,
-    sync::atomic::{AtomicU64, Ordering},
-};
+use std::sync::atomic::{AtomicU64, Ordering};
 
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 
 fn criterion_benchmark(criterion: &mut Criterion) {
-    criterion.bench_function("Box::new", |bencher| {
-        bencher.iter(|| {
-            black_box(|| {
-                drop(Box::new(1));
-            })();
-        });
-    });
-
-    criterion.bench_function("Box/64", |bencher| {
-        bencher.iter(|| {
-            black_box(|| {
-                for i in 0..64 {
-                    let _ = Box::new(i);
-                }
-            })();
-        });
-    });
-
-    criterion.bench_function("AtomicU64::fetch_or", |bencher| {
-        bencher.iter(|| {
-            black_box(|| {
-                AtomicU64::new(0).fetch_or(1, Ordering::AcqRel);
-            })();
-        });
-    });
-
     criterion.bench_function("AtomicU64/64", |bencher| {
         bencher.iter(|| {
-            black_box(|| {
-                let occupancy = AtomicU64::new(0);
+            let occupancy = AtomicU64::new(0);
 
-                let mut occupied: u64 = 0;
+            let mut occupied: u64 = 0;
 
-                loop {
-                    let least_significant_bit = !occupied & occupied.wrapping_add(1);
+            loop {
+                let least_significant_bit = !occupied & occupied.wrapping_add(1);
 
-                    if least_significant_bit.ne(&0) {
-                        occupied = occupancy.fetch_or(least_significant_bit, Ordering::AcqRel)
-                            | least_significant_bit;
-                    } else {
-                        break;
-                    }
+                if least_significant_bit.ne(&0) {
+                    occupied = occupancy.fetch_or(least_significant_bit, Ordering::AcqRel)
+                        | least_significant_bit;
+                } else {
+                    break;
                 }
-            })();
+            }
         });
     });
 
@@ -60,7 +29,7 @@ fn criterion_benchmark(criterion: &mut Criterion) {
             let slab: Fixed64<usize> = Fixed64::new();
 
             for i in 0..64 {
-                mem::forget(slab.get_uninit_slot().unwrap().insert(i));
+                black_box(slab.get_uninit_slot().unwrap().insert(i));
             }
         });
     });
@@ -71,49 +40,78 @@ fn criterion_benchmark(criterion: &mut Criterion) {
         bencher.iter(|| {
             let slab: Boxed64<usize> = Boxed64::new();
 
-            for i in 0..64 {
-                mem::forget(slab.get_uninit_slot().unwrap().insert(i));
-            }
+            black_box(
+                (0..64)
+                    .map(|i| slab.get_uninit_slot().unwrap().insert(i))
+                    .collect::<Vec<_>>(),
+            );
         });
     });
 
-    for n in 6..17 {
-        let batch_size: usize = 1 << n;
+    let mut alloc_bench = criterion.benchmark_group("Alloc");
 
-        criterion.bench_with_input(
-            BenchmarkId::new("Bump64", batch_size),
+    for n in 6..12 {
+        let batch_size: usize = 1 << n;
+        alloc_bench.bench_with_input(
+            BenchmarkId::new("Box<T>", batch_size),
             &batch_size,
             |b, batch_size| {
-                use arena64::arena::{Bump64, Slot};
-
                 b.iter(|| {
-                    let mut arena: Bump64<usize> = Bump64::new();
-
-                    let slots: Vec<Slot<usize>> =
-                        (0..*batch_size).map(|i| arena.insert(i)).collect();
-
-                    assert_eq!(slots, (0..*batch_size).collect::<Vec<usize>>());
+                    for i in 0..*batch_size {
+                        black_box(Box::new(i));
+                    }
                 });
             },
         );
 
-        criterion.bench_with_input(
+        alloc_bench.bench_with_input(
+            BenchmarkId::new("Box<Box<T>>", batch_size),
+            &batch_size,
+            |b, batch_size| {
+                b.iter(|| {
+                    for i in 0..*batch_size {
+                        black_box(Box::new(Box::new(i)));
+                    }
+                });
+            },
+        );
+
+        alloc_bench.bench_with_input(
+            BenchmarkId::new("Bump64", batch_size),
+            &batch_size,
+            |b, batch_size| {
+                use arena64::arena::Bump64;
+
+                b.iter(|| {
+                    let mut arena: Bump64<usize> = Bump64::new();
+                    black_box(
+                        (0..*batch_size)
+                            .map(|i| arena.insert(i))
+                            .collect::<Vec<_>>(),
+                    );
+                });
+            },
+        );
+
+        alloc_bench.bench_with_input(
             BenchmarkId::new("Arena64", batch_size),
             &batch_size,
             |b, batch_size| {
-                use arena64::arena::{Arena64, Slot};
+                use arena64::arena::Arena64;
 
                 b.iter(|| {
                     let arena: Arena64<usize> = Arena64::new();
-
-                    let slots: Vec<Slot<usize>> =
-                        (0..*batch_size).map(|i| arena.insert(i)).collect();
-
-                    assert_eq!(slots, (0..*batch_size).collect::<Vec<usize>>());
+                    black_box(
+                        (0..*batch_size)
+                            .map(|i| arena.insert(i))
+                            .collect::<Vec<_>>(),
+                    );
                 });
             },
         );
     }
+
+    alloc_bench.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
